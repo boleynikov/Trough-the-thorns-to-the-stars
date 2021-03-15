ebc7f5cea05602fb4904c878d8913655ae75ce49
boleynikov <ol.bogdan13@gmail.com>
2021-03-15 19:08:52 +0200

    Начальный этап - с готовым движением игрока

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    CircleCollider2D circleCollider;
+    public float movementX;
+    public float speed = 5f;
+    
+    
+    void Start()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+        rb = GetComponent<Rigidbody2D>();       
+    }
+    public void OnLeftZoneDown() {
+
+        movementX = -1;
+        Debug.Log("LeftTouch");
+    }
+    public void OnRightZoneDown() {
+
+        movementX = 1;
+        Debug.Log("RightTouch");
+    }
+    public void OnZoneUp() {
+        movementX = 0;
+    }
+    void Update()
+    {
+        rb.velocity = new Vector2(movementX * speed, rb.velocity.y);
+    }
+}
